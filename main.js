531c795702fd0d0767b5628536b12664f55afce9
Ren-Yoshino <140899767+Ren-Yoshino@users.noreply.github.com>
2025-04-19 22:19:21 -0300

    Ver 1.0
    modification in HTML and JS. 
    HTML: Add results field.
    JS: Add conversion and processing dates, end process return in result field in HTML file.

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let victoryStatus = [];
 let playerCount = 0;
 let enemyClanCount = 0;
 let playerViewCount = 0;
-
+//---------------------------------- Add Player and Enemy Clan ----------------------------------
 function addPlayerName() {
     let player = document.getElementById("playerName").value;
     if (player == "") {
@@ -102,7 +102,7 @@ function addNumberFieldCol() {
     let j = enemyClanCount - 1;
     for (let i = 1; i < table.rows.length - 1; i++) {
         //let newCol = document.createElement("td");
-        table.rows[i].insertCell(j+1);
+        table.rows[i].insertCell(j + 1);
         table.rows[i].cells[j + 1].innerHTML = "<input type='number' name='playerScore' class='scoreInput' id='playerScore" + i + "_" + (j) + "' value='0' min='0' max='25' step='1' oninput='this.value = Math.max(0, Math.min(25, this.value))' onblur='calcTotalKills(" + i + ")'>";
     }
     updateColSpan();
@@ -131,7 +131,7 @@ function updateColSpan() {
 function calcTotalKills(rowPosition) {
     let table = document.getElementById("resultTable");
     let totalKill = 0;
-    let j=0;
+    let j = 0;
     for (let i = 1; i < enemyClanCount + 1; i++, j++) {
         let score = document.getElementById("playerScore" + rowPosition + "_" + j).value;
         totalKill += parseInt(score);
@@ -141,3 +141,89 @@ function calcTotalKills(rowPosition) {
 }
 
 //----------------------------------------------------------------------------------------
+
+//------------------------------- End Result -----------------------------------
+function endResult() {
+    let table = document.getElementById("resultTable");
+    let result = "結果<br/>";
+    for (let i = 0; i < playerCount; i++) {
+        result += "<span style='font-weight: bold;'>" + players[i] + "</span><br>";
+        for (let j = 0; j < enemyClanCount; j++) {
+            let score = document.getElementById("playerScore" + (i + 1) + "_" + j).value;
+            result += enemyClan[j] + ": " + score + "\n";
+        }
+        let totalKill = document.getElementById("playerTotalKill" + (i + 1)).value;
+        result += "<span style='font-weight: bold;'>合計キル:</span> " + totalKill + "<br>";
+    }
+    document.getElementById("Result1").innerHTML = result; // Insert result in Result1;
+
+    const result2 = orderResult(); // Call orderResult function to get the ranking
+    document.getElementById("Result2").innerHTML = result2; // Insert result in Result2;
+    document.getElementById("Result3").innerHTML = orderResults(); // Insert result in Result3;
+}
+
+function orderResult() { //playerTotalKill1
+    let playerTotalKill = [];
+    let localPlayers = players;
+    let result = "順位：<br/>";
+    let position = 1;
+    let killValue = 0;
+    for (let i = 0; i < playerCount; i++) { // get Totatal Kill
+        playerTotalKill[i] = document.getElementById("playerTotalKill" + (i + 1)).value;
+    }
+    //-------------------------- Ordentae Result - using Buble Sort --------------------------
+    for (let outer = 0; outer < playerTotalKill.length - 1; outer++) {
+        for (let i = playerTotalKill.length - 1; i > outer; i--) {
+            if (playerTotalKill[i] > playerTotalKill[i - 1]) {
+                let tmp = playerTotalKill[i]; //using for playerTotalKill
+                playerTotalKill[i] = playerTotalKill[i - 1];
+                playerTotalKill[i - 1] = tmp;
+
+                let tmpPlayer = localPlayers[i]; // using for players
+                localPlayers[i] = localPlayers[i - 1];
+                localPlayers[i - 1] = tmpPlayer;
+            }
+        }
+    }
+    //-------------------------------End Buble Sort------------------------------
+    for (let i = 0; i < playerCount; i++) {
+        if (i == 0) {
+            result += position + "位: " + localPlayers[i] + " - " + playerTotalKill[i] + "<br>";
+            killValue = playerTotalKill[i];
+        }
+        else {
+            if (playerTotalKill[i] != killValue) {
+                position++;
+                killValue = playerTotalKill[i];
+            }
+            result += position + "位: " + localPlayers[i] + " - " + playerTotalKill[i] + "<br>";
+        }
+    }
+    return result; // Return the result string to be displayed in Result2
+}
+
+function orderResults(){
+    let result = "<span style='font-weight: bold;'> 観戦プレイヤー:</sapn><br/>";
+    for(let i = 0; i < playerViewCount; i++){
+        if(i+1 < playerViewCount){
+            result += playerViews[i] + ",";
+        }
+        else{
+            result += playerViews[i] + ";<br/>";
+        }
+    }
+    result += "<span style='font-weight: bold;'> 戦闘結果:</span><br/>" + (checkVictoryStatus());
+}
+
+function checkVictoryStatus(){
+    let victoryCount = 0, defeatCount = 0;
+    for(let i = 0; i < victoryStatus.length; i++){
+        if(victoryStatus[i] == "Win"){
+            victoryCount++;
+        }
+        else{
+            defeatCount++;
+        }
+    }   
+    return (victoryCount + defeatCount) + "戦中： <span style='color: green>" +victoryCount + "勝</span> <span style='color: red> " + defeatCount + " 敗</sapn> <br/>"; ;
+}
